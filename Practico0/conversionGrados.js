424e34aad84e3f7e8c6b2abd71f506588181a4e8
Mariana Tonda <marianapaolatonda@gmail.comgit config --globa user.email marianapaolatonda@gmail.com>
2023-05-23 21:50:29 -0700

    “Agrego

---
+++ Practico0/conversionGrados.js
@@ -0,0 +1,6 @@
+//Grados Celsius 1
+let celsius=(50-32)*5/9;
+console.log("50 grados Fahrenheit corresponden a: " + celsius + " grados Celsius");
+//Grados Celsius 2
+let celsius1=(75-32)*5/9;
+console.log("75 grados Fahrenheit corresponden a: " + celsius1 + " grados Celsius");
