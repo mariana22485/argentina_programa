f8322bb738784530db806fae5fef973a7eeac5eb
Mariana Tonda <marianapaolatonda@gmail.comgit config --globa user.email marianapaolatonda@gmail.com>
2023-05-23 21:57:25 -0700

    Practico N0 finalizado

---
+++ Practico0/areaRectangulo.js
@@ -0,0 +1,12 @@
+
+//Triangulo 1
+let base=5;
+let altura=3
+let area=base*altura;
+console.log("El area del triangulo 1 es: " + area);
+//Traingulo 2
+let base1=12.5;
+let altura1=5.3
+let area1=base1*altura1;
+console.log("El area del triangulo 2 es: " + area1);
+
---
+++ Practico0/promedio3.js
@@ -0,0 +1,18 @@
+//Promedio 1
+let num1=5;
+let num2=7;
+let num3=9;
+let Promedio1=(num1*num2*num3)/3;
+console.log("El promedio entre los tres numeros es:"  + Promedio1 );
+//Promedio 2
+let num4=-2;
+let num5=-3;
+let num6=-8;
+let Promedio2=(num4*num5*num6)/3;
+console.log("El promedio entre los tres numeros es:"  + Promedio2 );
+//Promedio 3
+let num7=-1.2;
+let num8=5.3;
+let num9=-3.3;
+let Promedio3=(num7*num8*num9)/3;
+console.log("El promedio entre los tres numeros es:"  + Promedio3 );
