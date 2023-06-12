57d1de93327dea4f080c7589cf18179d7bbcded1
Mariana Tonda <marianapaolatonda@gmail.comgit config --globa user.email marianapaolatonda@gmail.com>
2023-06-12 12:43:15 -0700

    agrego operacionesAritmeticas.js

---
+++ Practico1/operacionesAritmeticas.js
@@ -0,0 +1,19 @@
+let a=10
+let b=12
+let c=8
+let d=6
+
+let suma= a+b;
+let resta= c-d;
+
+let resultadoFinal= suma*resta
+console.log(resultadoFinal);
+
+//
+
+let esPar= resultadoFinal %2 === 0; 
+
+console.log("Mis variables iniciales fueron: " + a + ", " + b + ", " + c + " y " + d + ". La respuesta a verificar si el resultado final es par es: " + esPar);
+
+
+
