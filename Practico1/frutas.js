d48bc47496996a8b05cf4c4b8bf8bbc8df89d275
Mariana Tonda <marianapaolatonda@gmail.comgit config --globa user.email marianapaolatonda@gmail.com>
2023-06-12 12:45:30 -0700

    agrego frutas.js

---
+++ Practico1/frutas.js
@@ -0,0 +1,10 @@
+
+
+let frutas = [];
+frutas.push('manzana', 'frutilla', 'mango', 'melon', 'cereza');
+
+let ultima = frutas[frutas.length - 1];
+console.log("La última fruta agregada fue: " + ultima);
+
+let primera = frutas[0];
+console.log("La primera fruta agregada fue: " + primera);
