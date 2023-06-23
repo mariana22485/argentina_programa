e47ae8afa276ce160cfc88d7ad208f959570b0d8
Mariana Tonda <marianapaolatonda@gmail.comgit config --globa user.email marianapaolatonda@gmail.com>
2023-06-23 12:23:16 -0700

    Agrego lanzamiento

---
+++ Practico3/lanzamiento.js
@@ -0,0 +1,13 @@
+let i=10;
+while (i>=0){
+    if(i===10){
+        console.log("Cuenta regresiva %i",i);
+            }
+            else if(i===0){
+                console.log("Lanzamiento!");
+            }
+            else{
+                console.log('%i',i);
+            }
+            i--;
+}
